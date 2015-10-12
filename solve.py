38c180a1a9a6a76a8f7e81394d8c279bfaaf4573
Catalin Patulea <catalinp@google.com>
2015-10-12 01:21:36 -0400

    Replace sbag with pyelliptic, more self-contained.

--- solve.py
+++ solve.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python
-import itertools, os, subprocess, re, sys
+import binascii, ctypes, itertools, os, subprocess, re, sys
+from pyelliptic.openssl import OpenSSL
+
+OpenSSL.EC_POINT_point2hex = OpenSSL._lib.EC_POINT_point2hex
+OpenSSL.EC_POINT_point2hex.restype = ctypes.c_void_p
+OpenSSL.EC_POINT_point2hex.argtypes = [
+    ctypes.c_void_p, ctypes.c_void_p, ctypes.c_int, ctypes.c_void_p]
+POINT_CONVERSION_COMPRESSED = 2
+
+OpenSSL.CRYPTO_free = OpenSSL._lib.CRYPTO_free
+OpenSSL.CRYPTO_free.restype = None
+OpenSSL.CRYPTO_free.argtypes = [ctypes.c_void_p]
 
 def unlink_f(n):
   try:
@@ -10,26 +21,24 @@ def unlink_f(n):
 # parse
 bitcoin, wk, ciphertext = None, None, None
 for line in open('montrehack.flag.nc'):
-  m = re.search(r'\b(1\w+)\b', line)
-  if m:
-    bitcoin = m.group(1)
+  _, sep, rest = line.partition('cp=')
+  if sep:
+    cp = rest.rstrip()
 
-  m = re.search(r'wk=(\w+)', line)
-  if m:
-    wk = '0x' + m.group(1)
+  _, sep, rest = line.partition('wk=')
+  if sep:
+    wk = rest.rstrip()
 
 _, sep, ciphertext = open('montrehack.flag.nc').read().partition('\n.\n')
-assert sep
+assert sep and cp and wk and ciphertext
 
-assert bitcoin and wk and ciphertext
-
-print 'bitcoin=%s' % bitcoin
+print 'cp=%s' % cp
 print 'wk=%s' % wk
 
 # factor
 unlink_f('msieve.log')
 unlink_f('msieve.dat')
-subprocess.check_call(['../msieve/msieve', wk])
+subprocess.check_call(['../msieve/msieve', '0x' + wk])
 factors = []
 for line in open('msieve.log'):
   m = re.search(r'  ([cp])\d+ factor: (\d+)', line)
@@ -40,31 +49,45 @@ for line in open('msieve.log'):
 print '%d factors: %r' % (len(factors), factors)
 
 # check
-p = subprocess.Popen(
-  ['../sbag/sbag'],
-  stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 key, tried = None, 0
-for ii in itertools.product([1, 0], repeat=len(factors)):
+k = OpenSSL.EC_KEY_new_by_curve_name(OpenSSL.get_curve('secp256k1'))
+group = OpenSSL.EC_KEY_get0_group(k)
+for ff in itertools.product(*[[1, f] for f in factors]):
   a = 1
-  for i, f in zip(ii, factors):
-    if i:
-      a *= f
-  p.stdin.write('%x\n' % a)
-  addr = p.stdout.readline()
-  if not addr:
-    raise ValueError('sbag error')
+  for f in ff: a *= f
 
-  if addr.strip() == bitcoin:
-    key = '%032x' % a
-    print 'found it!', key
+  if a >= 2 ** 128:
+    continue
+
+  ahex = '%x' % a
+  if len(ahex) % 2: ahex = '0' + ahex
+  abin = binascii.unhexlify(ahex)
+
+  priv_key = OpenSSL.BN_bin2bn(abin, len(abin), 0)
+  pub_key = OpenSSL.EC_POINT_new(group)
+  assert OpenSSL.EC_POINT_mul(group, pub_key, priv_key, None, None, None) == 1
+
+  pubhex = OpenSSL.EC_POINT_point2hex(
+      group, pub_key, POINT_CONVERSION_COMPRESSED, 0)
+  if ctypes.cast(pubhex, ctypes.c_char_p).value == cp:
+    print 'found it!', ahex
+    key = ahex
     break
+
+  OpenSSL.CRYPTO_free(pubhex)
+  OpenSSL.EC_POINT_free(pub_key)
+  OpenSSL.BN_free(priv_key)
 
   tried += 1
   if tried % 10000 == 0:
     sys.stdout.write('%d.. ' % tried)
+    sys.stdout.flush()
+
+OpenSSL.EC_KEY_free(k)
 assert key
 
 # decrypt
+key = key.rjust(32, '0')
 p = subprocess.Popen([
   'mdecrypt', '--keymode', 'hex', '--keysize', '16', '--bare',
   '--key', key, '--mode', 'cbc',
